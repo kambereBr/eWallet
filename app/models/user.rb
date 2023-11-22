fcf08228fd4a4a762c068d7e19ee1f7376fc0843
Bruno Kambere <brunokambere@gmail.com>
2023-11-22 01:13:48 +0100

    Update User model by renaming user foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :groups
+  has_many :groups, class_name: 'Group', foreign_key: 'author_id'
   has_many :authored_operations, class_name: 'Operation', foreign_key: 'author_id'
 end
