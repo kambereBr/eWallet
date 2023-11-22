749777e4b19c19480a960f7d3b5030bc82aa217e
Bruno Kambere <brunokambere@gmail.com>
2023-11-22 01:12:37 +0100

    Update paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
-  root 'home#index'
   devise_for :users
+
+  unauthenticated do
+    root 'home#index', as: :unauthenticated_root
+  end
+
   devise_scope :user do
     authenticated :user do
       root to: "groups#index", as: :authenticated_root
     end
-
-    unauthenticated do
-      root to: "home#index", as: :unauthenticated_root
-    end
   end
 
   scope "/" do
-    resources :users, only: [:index, :show] do
-      resources :groups, only: [:index, :show, :new, :create] do
-        resources :operations, only: [:index, :show, :new, :create]
-      end
+    resources :users, only: [:index, :show]
+    resources :groups, only: [:index, :show, :new, :create] do
+      resources :operations, only: [:index, :show, :new, :create]
     end
   end
 end
