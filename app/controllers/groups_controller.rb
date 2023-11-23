f5c60f70f5ce36fc8f4f74a81e0627cf98ac0af8
Bruno Kambere <brunokambere@gmail.com>
2023-11-23 21:49:17 +0100

    Fix n+1 issues

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,14 @@
 class GroupsController < ApplicationController
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.includes(icon_attachment: :blob)
+    @user_operations = Operation.where(author_id: current_user.id)
+    @total_amount = @user_operations.sum(:amount)
   end
 
   def show
     @group = Group.find(params[:id])
+    @user_operation = @group.operations.where(author_id: current_user.id)
+    @group_amount = @user_operation.sum(:amount)
   end
 
   def new
